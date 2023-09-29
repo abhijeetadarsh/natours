6c77b1a8a529da35fac21a99b464f1fbfce66187
bird@kali <ad.abhijeetadarsh@gmail.com>
2023-09-30 03:01:33 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    // res.status(200).send('Hello from the server side');
+    res
+        .status(200)
+        .json({"message": 'Hello from the server side', "app":'natours'});
+        // ^ this by default set content type to json
+});
+
+app.post('/', (req, res) => {
+    res.send('You can post to this endpoint.....');
+})
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App is running on port ${port}`);
+});
