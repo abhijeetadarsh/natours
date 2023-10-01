1a98cd9f16b199ff344b1f59617bb9b03e56cd23
bird@kali <ad.abhijeetadarsh@gmail.com>
2023-10-01 16:15:34 +0530

    handled url parameters : implemented get a tour using id

--- app.js
+++ app.js
@@ -23,16 +23,44 @@ app.get('/api/v1/tours', (req, res) => {
         });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+    // console.log(req.params); // req.params store all the parameters of the url i.e. 'id' here
+
+    const id = Number(req.params.id);
+    // const id = req.params.id * 1; // another way do the same as above
+
+    const tour = tours.find(ele => ele.id === id);
+
+    // if(id >= tours.length || id < 0) {
+    if (!tour) {
+        return res
+            .status(404)
+            .json({
+                status: 'fail',
+                message: 'invalid id'
+            });
+    }
+
+    res
+        .status(200)
+        .json({
+            status: 'success',
+            data: {
+                tour
+            }
+        });
+});
+
 app.post('/api/v1/tours', (req, res) => {
     // console.log(req.body);
     // body should be a property of req because we used the middleware l.no 6
 
     const newId = tours.at(-1).id + 1;
     const newTour = Object.assign({ id: newId }, req.body);
-    
+
     tours.push(newTour);
     fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        if(err) return console.log(err.message);
+        if (err) return console.log(err.message);
         res
             .status(201)
             .json({
